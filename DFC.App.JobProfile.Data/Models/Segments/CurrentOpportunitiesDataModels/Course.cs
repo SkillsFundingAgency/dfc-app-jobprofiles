a7121644b5735858e432c6a8e904cbf4d398f071
Ian Crisp <icrisp@program-systems.co.uk>
2019-09-20 10:47:41 +0100

    Added Current opportunities segment data model. Removed duplicate display of fields in view

---
+++ DFC.App.JobProfile.Data/Models/Segments/CurrentOpportunitiesDataModels/Course.cs
@@ -0,0 +1,15 @@
+﻿namespace DFC.App.JobProfile.Data.Models.Segments.CurrentOpportunitiesDataModels
+{
+    public class Course
+    {
+        public string Title { get; set; }
+
+        public string CourseId { get; set; }
+
+        public string Provider { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string Location { get; set; }
+    }
+}

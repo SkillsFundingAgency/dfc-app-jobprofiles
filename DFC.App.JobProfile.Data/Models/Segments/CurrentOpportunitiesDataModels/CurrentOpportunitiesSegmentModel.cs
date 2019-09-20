191d3b52e7b54c70119881606f4a5eac5821b543
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2019-09-20 10:50:26 +0100

    Added Current opportunities segment data model. (#20)
    Removed duplicate display of fields in view

--- DFC.App.JobProfile.Data/Models/Segments/CurrentOpportunitiesDataModels/CurrentOpportunitiesSegmentModel.cs
+++ DFC.App.JobProfile.Data/Models/Segments/CurrentOpportunitiesDataModels/CurrentOpportunitiesSegmentModel.cs
@@ -1,7 +1,19 @@
-﻿namespace DFC.App.JobProfile.Data.Models.Segments.CurrentOpportunitiesDataModels
+﻿using System.Collections.Generic;
+
+namespace DFC.App.JobProfile.Data.Models.Segments.CurrentOpportunitiesDataModels
 {
     public class CurrentOpportunitiesSegmentModel : BaseSegmentModel
     {
-        public string Markup { get; set; }
+        public string JobTitle { get; set; }
+
+        public IEnumerable<string> Standards { get; set; }
+
+        public IEnumerable<string> Frameworks { get; set; }
+
+        public string CourseKeywords { get; set; }
+
+        public IEnumerable<Apprenticeship> Apprenticeships { get; set; }
+
+        public IEnumerable<Course> Courses { get; set; }
     }
 }

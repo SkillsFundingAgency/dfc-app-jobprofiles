b86eb24643c2668f269e00cf34d1ff9b0dff0e1a
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2019-12-10 12:31:33 +0000

    Changed code to use govuk css class for input box. (#82)
    * Changed code to use govuk css class for input box.
    Changed code to prevent redirect to search results when no search term is entered, and so stays on job profile page.
    
    * Remove async
    
    * corrected broken tests

---
+++ DFC.App.JobProfile.UnitTests/ControllerTests/ProfileControllerTests/ProfileControllerSearchTests.cs
@@ -0,0 +1,55 @@
+﻿using DFC.App.JobProfile.Controllers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using Xunit;
+
+namespace DFC.App.JobProfile.UnitTests.ControllerTests.ProfileControllerTests
+{
+    [Trait("Profile Controller", "HTML Search Tests")]
+    public class ProfileControllerSearchTests : BaseProfileController
+    {
+        [Fact]
+        public void JobProfileControllerSearchRedirectsToSearchResults()
+        {
+            // Arrange
+            const string jobProfileUrl = "an-article";
+            const string searchTerm = "search term";
+            var controller = BuildProfileController(MediaTypeNames.Text.Html);
+
+            // Act
+            var result = controller.Search(jobProfileUrl, searchTerm);
+
+            // Assert
+            var statusResult = Assert.IsType<RedirectResult>(result);
+
+            statusResult.Url.Should().NotBeNullOrWhiteSpace();
+            statusResult.Url.Should().StartWith("/search-results?");
+            A.Equals(false, statusResult.Permanent);
+
+            controller.Dispose();
+        }
+
+        [Fact]
+        public void JobProfileControllerSearchRedirectsToSelfForNullSearchTerm()
+        {
+            // Arrange
+            const string jobProfileUrl = "an-article";
+            const string searchTerm = null;
+            var controller = BuildProfileController(MediaTypeNames.Text.Html);
+
+            // Act
+            var result = controller.Search(jobProfileUrl, searchTerm);
+
+            // Assert
+            var statusResult = Assert.IsType<RedirectResult>(result);
+
+            statusResult.Url.Should().NotBeNullOrWhiteSpace();
+            statusResult.Url.Should().StartWith($"/{ProfileController.ProfilePathRoot}/");
+            A.Equals(false, statusResult.Permanent);
+
+            controller.Dispose();
+        }
+    }
+}

ede3bc32365255090e58631024002aa60078a96b
Muthu <muthu@lightningcloudconsultancy.com>
2020-07-24 19:08:56 +0100

    fix

--- DFC.App.JobProfile/Controllers/ProfileController.cs
+++ DFC.App.JobProfile/Controllers/ProfileController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DFC.App.JobProfile.Controllers

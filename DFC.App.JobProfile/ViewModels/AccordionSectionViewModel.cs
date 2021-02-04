6d4c2f21fb49ae1209e351ed0a25d95e71eb20fc
Ian Crisp <icrisp@program-systems.co.uk>
2021-02-04 07:30:45 +0000

    Split accordion sections into view model and shared .partial view

---
+++ DFC.App.JobProfile/ViewModels/AccordionSectionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace DFC.App.JobProfile.ViewModels
+{
+    public class AccordionSectionViewModel
+    {
+        public int SequenceNo { get; set; }
+
+        public string Title { get; set; }
+
+        public string Summary { get; set; }
+
+        public HtmlString Content { get; set; }
+    }
+}

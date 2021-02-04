df10c59b3edecb43ccb5fb754a9109047983a840
Ian Crisp <49161034+IanCrisp@users.noreply.github.com>
2021-02-04 08:17:15 +0000

    Story ft1 38 add accordions display (#170)
    * Remove segments links
    
    * Add accordion wrappers to segment contents
    
    * Split accordion sections into view model and shared
    .partial view
    
    * Removed view model from code coverage analysis

---
+++ DFC.App.JobProfile/ViewModels/AccordionSectionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.App.JobProfile.ViewModels
+{
+    [ExcludeFromCodeCoverage]
+    public class AccordionSectionViewModel
+    {
+        public int SequenceNo { get; set; }
+
+        public string Title { get; set; }
+
+        public string Summary { get; set; }
+
+        public HtmlString Content { get; set; }
+    }
+}

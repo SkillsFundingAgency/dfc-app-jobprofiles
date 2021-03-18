15434d386f01a2fab0746b960c8c97c8974d6b58
Ismail Lambat <lambat_15@hotmail.com>
2021-03-18 11:29:16 +0000

    Creation of the JobProfile UI Automation framework project within the solution

---
+++ DFC.App.JobProfile.UI.FunctionalTests/StepDefinitions/BasicSteps.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="BasicSteps.cs" company="National Careers Service">
+// Copyright (c) National Careers Service. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using DFC.TestAutomation.UI.Extension;
+using OpenQA.Selenium;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace DFC.App.JobProfile.UI.FunctionalTests.StepDefinitions
+{
+    [Binding]
+    internal class BasicSteps
+    {
+        public BasicSteps(ScenarioContext context)
+        {
+            this.Context = context;
+        }
+
+        private ScenarioContext Context { get; set; }
+
+    }
+}

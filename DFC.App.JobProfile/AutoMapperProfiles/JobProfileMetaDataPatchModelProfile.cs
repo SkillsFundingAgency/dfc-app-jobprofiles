f383fecc0bdf45a7353b5ca2a66ad1b684d9cf43
Lea Hayes <lea.hayes@education.gov.uk>
2023-09-27 15:48:52 +0100

    Do not exclude `SocialProofVideo` from data mapping.

--- DFC.App.JobProfile/AutoMapperProfiles/JobProfileMetaDataPatchModelProfile.cs
+++ DFC.App.JobProfile/AutoMapperProfiles/JobProfileMetaDataPatchModelProfile.cs
@@ -15,7 +15,6 @@ namespace DFC.App.JobProfile.AutoMapperProfiles
                 .ForMember(d => d.SocLevelTwo, s => s.Ignore())
                 .ForMember(d => d.LastReviewed, s => s.Ignore())
                 .ForMember(d => d.Segments, s => s.Ignore())
-                .ForMember(d => d.SocialProofVideo, s => s.Ignore())
                 ;
         }
     }

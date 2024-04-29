ae1189c64a00c1af09bacb57c16a2f1a54c10729
hnandra <32364605+hnandra@users.noreply.github.com>
2024-04-29 20:17:51 +0100

    Added custom resolvers

---
+++ DFC.App.JobProfile/AutoMapperProfiles/CustomResolvers/LocationResolver.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using DFC.App.JobProfile.Data.Models.Segment.Tasks;
+using DFC.App.JobProfile.Helpers;
+using DFC.App.JobProfile.ProfileService.Utilities;
+using DFC.Common.SharedContent.Pkg.Netcore.Model.Response;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DFC.App.JobProfile.AutoMapperProfiles.CustomResolvers
+{
+    public class LocationResolver : FormatContentService, IValueResolver<JobProfileWhatYoullDoResponse, TasksSegmentDataModel, string>
+    {
+        public string Resolve(
+            JobProfileWhatYoullDoResponse source,
+            TasksSegmentDataModel destination,
+            string destMember,
+            ResolutionContext context)
+        {
+            var wordList = new List<string>();
+
+            if (source.JobProfileWhatYoullDo.IsAny())
+            {
+                var responseData = source.JobProfileWhatYoullDo.FirstOrDefault();
+
+                if (responseData.RelatedLocations.ContentItems.IsAny())
+                {
+                    foreach (var contentItem in responseData.RelatedLocations.ContentItems)
+                    {
+                        wordList.Add(contentItem.Description);
+                    }
+
+                    return Convert(wordList);
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public string Convert(IEnumerable<string> wordList)
+        {
+            const string LocationLeadingText = "You could work";
+            const string LocationConjunction = "or";
+
+            return GetParagraphText(LocationLeadingText, wordList, LocationConjunction);
+        }
+    }
+}

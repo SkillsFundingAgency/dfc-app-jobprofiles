4f0e4a2d8095ac3ff72b83100add5dc8be960211
James <152189499+JamesPrior-07@users.noreply.github.com>
2024-05-03 17:40:12 +0100

    Remove unncessary usings

--- DFC.App.JobProfile.Data/Models/SkillsModels/JobProfileSkillSegmentDataModel.cs
+++ DFC.App.JobProfile.Data/Models/SkillsModels/JobProfileSkillSegmentDataModel.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NHibernate.Criterion;
 
 namespace DFC.App.JobProfile.Data.Models.SkillsModels
 {

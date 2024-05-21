04d751879257fd443a9a3f3a082001e916809232
Zhaoming LU <Zhaoming.LU@EDUCATION.GOV.UK>
2024-05-21 15:43:10 +0100

    Add more unit tests

---
+++ DFC.App.JobProfile.UnitTests/ControllerTests/ProfileControllerTests/ProfileControllerGetCountJobProfilesTests.cs
@@ -0,0 +1,34 @@
+﻿using DFC.App.JobProfile.Data.Models;
+using DFC.App.JobProfile.Data.Models.CurrentOpportunities;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.App.JobProfile.UnitTests.ControllerTests.ProfileControllerTests
+{
+    [Trait("Profile Controller", "Create or Update Tests")]
+    public class ProfileControllerGetCountJobProfilesTests : BaseProfileController
+    {
+        [Theory]
+        [MemberData(nameof(JsonMediaTypes))]
+        public async Task ProfileControllerGetCountJobProfilesReturnsSuccessForUpdate(string mediaTypeName)
+        {
+            // Arrange
+            int num = 1;
+            var controller = BuildProfileController(mediaTypeName);
+
+            A.CallTo(() => FakeJobProfileService.CountJobProfiles(A<string>.Ignored)).Returns(num);
+
+            // Act
+            var result = await controller.CountJobProfiles();
+
+            // Assert
+            var statusCodeResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal((int)HttpStatusCode.OK, statusCodeResult.StatusCode);
+
+            controller.Dispose();
+        }
+    }
+}
